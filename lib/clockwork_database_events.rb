8202b1de4969cbce8819a6684da225d34b45cabc
Martin Englund <martin@englund.nu>
2015-06-01 22:20:43 -0700

    add connection helper

--- lib/clockwork_database_events.rb
+++ lib/clockwork_database_events.rb
@@ -1,3 +1,6 @@
+require 'sequel'
+require 'faraday'
+require 'diplomat'
 require 'clockwork_database_events/version'
 
 # clockwork database events
@@ -11,4 +14,36 @@ module ClockworkDatabaseEvents
   end
 
   module_function :migrate
+
+  def connect(db_url=url)
+    Sequel.connect(db_url)
+  end
+
+  module_function :connect
+
+  def url
+    if ENV['SKALERA_DB_URL']
+      ENV['SKALERA_DB_URL']
+    elsif ENV['JENKINS_HOME']
+      'sqlite:/' # for running tests in jenkins
+    else
+      # TODO: user, password & db should be retrieved from consul
+      "postgres://skalera:skalera@#{host}/events"
+    end
+  end
+
+  module_function :url
+
+  def host
+    consul = ENV['CONSUL'] || 'consul'
+    Diplomat.configuration.url = "http://#{consul}:8500"
+
+    postgres_config = Diplomat::Service.get('postgres')
+    postgres_config.Address
+  rescue Faraday::ConnectionFailed => e
+    puts "unable to lookup '#{consul}': #{e.message}"
+    raise e
+  end
+
+  module_function :host
 end
--- spec/clockwork_database_events_spec.rb
+++ spec/clockwork_database_events_spec.rb
@@ -4,4 +4,8 @@ describe ClockworkDatabaseEvents do
   it 'has a version number' do
     expect(ClockworkDatabaseEvents::VERSION).not_to be nil
   end
+
+  it 'should connect to the database' do
+    expect(ClockworkDatabaseEvents.connect('sqlite:/')).to be_an_instance_of(Sequel::SQLite::Database)
+  end
 end
